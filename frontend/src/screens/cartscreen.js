ee6c9fce2d1b468ef05dcb18827202b2effebfc0
Aabha <aabhasoni500@gmail.com>
2022-05-24 22:08:30 +0530

    style

--- frontend/src/screens/cartscreen.js
+++ frontend/src/screens/cartscreen.js
@@ -68,7 +68,9 @@ const CartScreen = {
                 }
                 const cartItems = getCartItems();
                 return `
+                <div class="cartLink">
                 <a href="/#/" class="cartLink"><i class="fa fa-angle-double-left"></i>Go to home screen</a>
+                </div>
                     <div class="content cart">
                         <div class="cartList">
                             <ul class="cartListContainer">
